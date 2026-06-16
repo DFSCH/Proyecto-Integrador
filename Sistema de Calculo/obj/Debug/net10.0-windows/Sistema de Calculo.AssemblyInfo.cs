7b171a804417b1104d057f60af6cc2889684e684
Diego <diegofernando1522@gmail.com>
2026-06-15 22:29:35 -0500

    final

--- Sistema de Calculo/obj/Debug/net10.0-windows/Sistema de Calculo.AssemblyInfo.cs
+++ Sistema de Calculo/obj/Debug/net10.0-windows/Sistema de Calculo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema de Calculo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ca12a6afccabfc67d5279d7988d74439b71129")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema de Calculo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema de Calculo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
